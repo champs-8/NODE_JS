67103d8588215741a0953c3926fbca7d9f0974c7
champs-8 <https://github.com/champs-8/Projetos>
2022-12-13 22:04:31 -0300

    pegando valores do DB

--- app.js
+++ app.js
@@ -3,6 +3,37 @@ const express = require("express");
 //instanciando a funcão express
 const app = express();
 
+//criando conexão com DB mysql
+const mysql =  require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "champs"
+});
+
+//vai mostrar se foi conectado ou não
+//exemplo da net
+connection.connect((err) => {
+    if (err) {
+        console.error(`Erro de conexão: ${err.stack}`);
+        return
+    }
+    console.log(`Conectado no id ${connection.threadId}`);
+})
+
+//pegar dados do DB
+//mesma query para pegar informações no workbench
+connection.query('select * from users', (err, res, fields) => {
+    //se nao teve nenhum erro, vai mostrar o resultado
+    if(!err) {
+        console.log(`RESULTADO: ${res}`);
+    }else{
+        console.error("ERRO na consulta");
+    }
+
+})
+
 
 //qual pagina quero abrir
 //vou abrir agora um arquivo ao inves de uma frase.
