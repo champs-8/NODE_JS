a6ff9e5a94da624dbff24eabe0aa6fe91e46da91
champs-8 <https://github.com/champs-8/Projetos>
2022-12-15 20:58:30 -0300

    instalando e usando sequelize

--- app.js
+++ app.js
@@ -1,99 +1,47 @@
-//incluindo modulos
-const express = require("express");
-//instanciando a funcão express
-const app = express();
+//requerindo o modulo
+const Sequelize = require('sequelize');
 
-//criando conexão com DB mysql
-const mysql =  require("mysql");
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "champs"
+//
+const sequelize = new Sequelize('champs', 'root', '', {
+    host : 'localhost',
+    dialect : 'mysql'
 });
 
-//vai mostrar se foi conectado ou não
-//exemplo da net
-connection.connect((err) => {
-    if (err) {
-        console.error(`Erro de conexão: ${err.stack}`);
-        return
-        //quando tiver erro, vai retornar e nao vai prosseguir com o ID;
+//testando a conexão
+//then vai receber a conxão com sucesso,
+//catch é quando nao receber
+sequelize.authenticate().then(() => {
+    console.log('Conexão realizada com sucesso');
+}).catch((err) => {
+    console.error(`Conexão não realizada. ERRO: ${err} `);
+});
+
+//sequelize.model ou define  
+
+const Pagamento = sequelize.define('pagamentos', {
+    //atributos
+    //criando a tabela pagamentos utilizando define
+    nome: {
+        type: Sequelize.STRING
+    },
+    valor: {
+        type: Sequelize.DOUBLE
     }
-    console.log(`Conectado no id ${connection.threadId}`);
-})
+    //criou tambem as colunas ID, createAt e updateAt
+});
+
+//para criar a tabela usa sync
+//passa obejto force: true como parametro para forçar a criação da tabela.
 
 
-//query para inserir dados no DB champs; pois a credencial connection
-//acessa ele.
+//criar tabela com sequelize.
+//User.sync({force: true});     ****
 
-// connection.query("insert into users values (default,'Ana','ana@email.com'),(default,'Irineu','irineu@emial.com');", (err, res) => {
-//     //nao teve erro.
-//     //nessa conexão, o comando sql funcionou
-//     if(!err) {
-//         console.log('Usuário Cadastrado com sucesso!');
-//     }else{
-//         console.log('Erro ao cadastrar usuário.');
-//     }
-// })
-
-// =====================================
-    
-//fazer alterações no DB
-//modo de inserção correta
-// connection.query("update users set nome = 'Oswanildo', email = 'oswanildo@email.com' where id = 7;", (err, res) => {
-//     if(!err) {
-//         console.log('Usuario editado com sucesso');
-//     }else{
-//         console.error('Erro: o usuário nao foi editado com sucesso.');
-//     }
-// });
-
-// =====================================
-
-//deletar dados do DB
-//sintaxe correta
-connection.query("delete from users where id = 6;", (err, res) => {
-    if (!err) {
-        console.log('Linha de informação removida.');
-    }else{
-        console.error('Erro ao remover linha de informação.');
-    }
-})
-
-
-//pegar dados do DB
-//mesma query para pegar informações no workbench
-connection.query('SELECT * FROM users', (err, rows, fields) => {
-    //se nao teve nenhum erro, vai mostrar o resultado
-    if(!err) {
-        console.log(`RESULTADO: `, rows);
-        //(`REsultado: ${rows}`) dá errado.
-    }else{
-        console.error("ERRO na consulta");
-    }
-})
-
-
-//qual pagina quero abrir
-//vou abrir agora um arquivo ao inves de uma frase.
-app.get("/", (req, res) => {
-    //res.send("Gerenciador de putas");
-    //vai pegar o diretório até chegar no app.js
-    //depois concatenado com o restante do texto, carrega o arquivo.
-
-    res.sendFile(__dirname + '/src/index.html')
-})
-app.get("/sobre-empresa", (req, res) => {
-    res.sendFile(`${__dirname}/src/sobre-empresa.html`)
-})
-app.get("/blog", (req, res) => {
-    res.send("Página do blog")
-})
-app.get("/contato", (req, res)=> {
-    res.send("Página de contato")
-})
-//iniciar um servidor
-app.listen(8080);
-//localhost:8080
-//na pasta raiz digita node index.js
+//criando novos valores na tabela pagamentos.
+//mesmo definindo outra coluna no create, nao carrega
+//???? siginifica que pegou as informações passadas e as automáticas.
+Pagamento.create({
+    nome: 'energia',
+    valor: 80,
+    tipo_Pagamento: 'pix'
+});
--- rotas/app.js
+++ rotas/app.js
@@ -3,6 +3,25 @@ const express = require("express");
 //instanciando a funcão express
 const app = express();
 
+//criando conexão com DB mysql
+const mysql =  require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "champs"
+});
+
+//vai mostrar se foi conectado ou não
+//exemplo da net
+connection.connect((err) => {
+    if (err) {
+        console.error(`Erro de conexão: ${err.stack}`);
+        return
+        //quando tiver erro, vai retornar e nao vai prosseguir com o ID;
+    }
+    console.log(`Conectado no id ${connection.threadId}`);
+});
 
 //qual pagina quero abrir
 //vou abrir agora um arquivo ao inves de uma frase.
@@ -14,7 +33,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
 })
 app.get("/sobre-empresa", (req, res) => {
-    res.send("Página sobre empresa")
+    res.sendFile(`${__dirname}/src/sobre-empresa.html`)
 })
 app.get("/blog", (req, res) => {
     res.send("Página do blog")
@@ -23,6 +42,6 @@ app.get("/contato", (req, res)=> {
     res.send("Página de contato")
 })
 //iniciar um servidor
-app.listen(8081);
-//localhost:8081
-//na pasta raiz digita node index.js
+app.listen(8080);
+//localhost:8080
+//na pasta raiz digita node app.js
